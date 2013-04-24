e5737762f29efc59ff3579a2b13434dae7657cd5
jan-ruzicka <ruzicka.jan@gmail.com>
2013-04-24 16:07:51 -0400

    version bump

--- ccccdist/cccc/cccc_ver.h
+++ ccccdist/cccc/cccc_ver.h
@@ -1,3 +1,3 @@
-// This version built on  at Fri 01/06/2006 
-#define CCCC_VERSION 3.1.5 
-#define CCCC_VERSION_STRING "3.1.5" 
+// This version built on  at Wed 2013/04/24
+#define CCCC_VERSION 3.2.1 
+#define CCCC_VERSION_STRING "3.2.1" 
